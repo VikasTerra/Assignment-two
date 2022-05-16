2d8b6555b48aa0504fcf6712d1516d66aaa06589
vkterra <vikas.k@terrapay.com>
2022-05-16 17:34:13 +0530

    Assignment two

--- product-service/src/main/java/com/example/controller/ProductController.java
+++ product-service/src/main/java/com/example/controller/ProductController.java
@@ -13,24 +13,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.entity.EmailDetails;
 import com.example.entity.Product;
 import com.example.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 	@Autowired
 	ProductService productService;
+	RestTemplate restTemplate;
 	
 	@PostMapping
 	public String createProduct(@Valid @RequestBody Product product) throws Exception {
 		productService.saveProduct(product);
+		createAndSendProductDetails(product);
 		return product.getId();
 	}
 	@GetMapping("/{id}")
 	ResponseEntity<Product> getProduct(@PathVariable String id) {
 		Optional<Product> product = productService.getProduct(id);
+		
 		return generateResponse(product);
 	}
 	@GetMapping("/name/{name}")
@@ -40,6 +46,25 @@ public class ProductController {
 	}
 	private ResponseEntity<Product> generateResponse(Optional<Product> product) {
 		return product.isPresent() ? new ResponseEntity(product, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	private ResponseEntity<String> callmeforCreateProduct(@RequestBody EmailDetails emailDeatails) {
+		if (restTemplate == null) {
+			restTemplate = new RestTemplate();
+		}
+		ResponseEntity<String> sys = restTemplate.postForEntity("http://'product-service':8989/api/sendMail",
+				emailDeatails, String.class);
+		return sys;
+	}
+	
+	private ResponseEntity<String> createAndSendProductDetails(Product product) throws Exception {
+		EmailDetails emailDetails = new EmailDetails();
+		ObjectMapper mapper = new ObjectMapper();
+		emailDetails.setMsgBody(mapper.writeValueAsString(product));
+		emailDetails.setRecipient("vikasblr95@gmail.com");
+		emailDetails.setSubject("Email Details");
+		ResponseEntity<String> response = callmeforCreateProduct(emailDetails);
+		return response;
 	}
 
 }
